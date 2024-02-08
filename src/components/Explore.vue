deaea584aadacced1ceec9fff8f6fc30ae540c06
Devin Gray <admin@devingray.io>
2024-02-07 16:46:18 -0800

    Basic transition animations

--- src/components/Explore.vue
+++ src/components/Explore.vue
@@ -1,18 +1,38 @@
 <template>
-	<div
-		@click.self="$emit('close')"
-		class="fixed inset-0 z-0"
-	>	</div>
-
-		<section id="explore" class="z-1 relative xl:col-start-2 xl:row-start-7 xl:col-span-2 xl:row-span-6 md:col-start-3 md:row-start-6 md:col-span-4 sm:col-span-6 sm:col-start-1 sm:row-span-6 col-span-6 bg-white flex flex-col justify-center p-12 max-w-[690px] max-h-[580px]">
-			<div
-				@click="$emit('close')"
-				class="text-[2.25rem] text-primary absolute top-5 right-5 p-2 cursor-pointer"
-			>
-				&times;
-			</div>
-			<h3 class="font-didot text-[2.625rem] font-bold text-left text-primary pb-[1.625rem] md:text-[4.125rem]">Explore</h3>
-			<p class="overflow-auto text-body font-lato text-[1.125rem] font-normal leading-[1.375rem] tracking-[0.3499999940395355px] text-left mb-[1.875rem]">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? </p>
-			<button class="bg-gradient-to-r from-primary to-secondary text-white uppercase w-[235px] h-[55px] p-2">Read More</button>
-		</section>
+	<section
+		id="explore"
+		class="absolute z-1 xl:col-start-2 xl:row-start-7 xl:col-span-2 xl:row-span-6 md:col-start-3 md:row-start-10 md:col-span-4 sm:col-span-6 sm:col-start-1 sm:row-span-6 col-span-6 bg-white flex flex-col justify-center p-12 max-w-[690px] max-h-[580px]"
+	>
+		<div
+			@click="$emit('close')"
+			class="z-5 text-[2.25rem] text-primary absolute top-5 right-5 p-2 cursor-pointer"
+		>
+			&times;
+		</div>
+		<h3
+			class="font-didot text-[2.625rem] font-bold text-left text-primary pb-[1.625rem] md:text-[4.125rem]"
+		>
+			Explore
+		</h3>
+		<p
+			class="overflow-auto text-body font-lato text-[1.125rem] font-normal leading-[1.375rem] tracking-[0.3499999940395355px] text-left mb-[1.875rem]"
+		>
+			Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+			doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo
+			inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+			Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
+			fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem
+			sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit
+			amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora
+			incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad
+			minima veniam, quis nostrum exercitationem ullam corporis suscipit
+			laboriosam, nisi ut aliquid ex ea commodi consequatur?
+		</p>
+		<button
+			class="bg-gradient-to-r from-primary to-secondary text-white uppercase w-[235px] h-[55px] p-2"
+		>
+			Read More
+		</button>
+	</section>
 </template>
+<style scoped></style>
--- src/components/MoreDetails.vue
+++ src/components/MoreDetails.vue
@@ -3,32 +3,65 @@ import { ref } from "vue";
 import Explore from "./Explore.vue";
 
 const showExplore = ref(false);
-const toggleExplore = () => (showExplore.value = !showExplore.value);
+const onExplore = () => (showExplore.value = true);
+const offExplore = () => (showExplore.value = false);
 </script>
 
 <template>
-	<section
-		v-if="!showExplore"
-		id="more-details"
-		class="col-start-2 col-span-6 row-start-6 row-span-2 flex flex-col justify-start rounded transition-colors duration-300 md:row-start-10 md:row-span-2 xl:col-start-2 xl:col-span-6"
-	>
-		<h2
-			class="font-ditot text-7xl font-bold leading-tight tracking-tight text-left text-white m-0 md:text-11xl md:leading-none xl:text-14xl"
+	<Transition name="fade">
+		<section
+			v-show="!showExplore"
+			id="more-details"
+			class="absolute col-start-2 col-span-6 row-start-6 row-span-2 flex flex-col justify-start rounded transition-colors duration-300 md:row-start-10 md:row-span-2 xl:col-start-2 xl:col-span-6"
 		>
-			Explore
-		</h2>
-		<div
-			class="flex justify-start items-center cursor-pointer"
-			@click="toggleExplore"
-		>
-			<img
-				src="../assets/Plus.svg"
-				class="w-full max-w-[2.6875rem] h-auto pr-4"
-			/>&nbsp;<strong
-				class="font-lato text-[1.5625rem] font-medium tracking-[0.625px] text-center text-white md:text-[1.5rem] md:leading-[1.875rem]"
-				>More Details</strong
+			<h2
+				class="font-ditot text-7xl font-bold leading-tight tracking-tight text-left text-white m-0 md:text-11xl md:leading-none xl:text-14xl"
 			>
-		</div>
-	</section>
-	<Explore v-if="showExplore" @close="showExplore = false" />
+				Explore
+			</h2>
+			<div
+				class="flex justify-start items-center cursor-pointer"
+				@click="onExplore"
+			>
+				<img
+					src="../assets/Plus.svg"
+					class="w-full max-w-[2.6875rem] h-auto pr-4"
+				/>&nbsp;<strong
+					class="font-lato text-[1.5625rem] font-medium tracking-[0.625px] text-center text-white md:text-[1.5rem] md:leading-[1.875rem]"
+					>More Details</strong
+				>
+			</div>
+		</section>
+	</Transition>
+	<Transition name="slide-fade">
+		<Explore v-show="showExplore" @close="offExplore" />
+	</Transition>
 </template>
+<style scoped>
+.fade-enter-from,
+.fade-leave-to {
+	opacity: 0;
+}
+.fade-enter-to,
+.fade-leave-from {
+	opacity: 1;
+}
+.fade-enter-active,
+.fade-leave-active {
+	transition: opacity 1s;
+}
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+	transform: scale(0);
+	transform-origin: bottom left;
+}
+.slide-fade-enter-to,
+.slide-fade-leave-from {
+	transform: scale(1);
+	transform-origin: bottom left;
+}
+.slide-fade-enter-active,
+.slide-fade-leave-active {
+	transition: transform 0.5s;
+}
+</style>
